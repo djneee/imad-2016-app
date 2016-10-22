e35e84f868f6a0355207d6a030f3a481055f1ae5
djneee <djneee@gmail.com>
2016-10-22 07:24:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/page1', function (req, res) {
-  res.sendFile('Page 1 is displayed ');
+  res.send('Page 1 is displayed ');
 });
 
 
